89e823f2f38db7f276be2034a9c538e9eeff6ae7
JayMar921 <jayharronabejar@gmail.com>
2023-10-05 21:29:42 +0800

    Added Controllers, no functions yet

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Cookie_Authentication_ASP.NET_Core_Web_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : Controller
+    {
+        
+    }
+}
